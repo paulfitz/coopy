2fce1235409a9212efd261c5791e08032ca421d3
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2010-03-25 12:16:36 -0400

    more meaningful version info, rename binaries

--- sheet_scm/src/fossil/VERSION.h
+++ sheet_scm/src/fossil/VERSION.h
@@ -1,4 +1,4 @@
 // need to automate this as per main.mk
-#define MANIFEST_UUID "notset"
-#define MANIFEST_VERSION "notset"
-#define MANIFEST_DATE "not set"
+#define MANIFEST_UUID "fossily"
+#define MANIFEST_VERSION "coopy_011"
+#define MANIFEST_DATE "2010/03/25"
