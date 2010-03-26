3f96fd824fab2a7934025d01a56934e9873acdb7
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2010-03-25 22:54:08 -0400

    checked in too much

--- build/sheet_scm/delta.h
+++
@@ -1,8 +0,0 @@
-/* This file was automatically generated.  Do not edit! */
-int delta_apply(const char *zSrc,int lenSrc,const char *zDelta,int lenDelta,char *zOut);
-int delta_output_size(const char *zDelta,int lenDelta);
-int delta_create(const char *zSrc,unsigned int lenSrc,const char *zOut,unsigned int lenOut,char *zDelta);
-typedef unsigned short int u16;
-typedef short int s16;
-typedef unsigned int u32;
-#define INTERFACE 0
