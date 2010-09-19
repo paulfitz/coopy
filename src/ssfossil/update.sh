e469b4c9e847b3b866cbf6d702725615e965111d
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2010-09-18 23:38:16 -0400

    prepare for being more systematic about fossil upgrades

---
+++ src/ssfossil/update.sh
@@ -0,0 +1,32 @@
+
+# My memory of how to update fossil
+
+if [ ! -e original.fsl ] ; then
+    fossil clone http://www.fossil-scm.org/fossil original.fsl
+fi
+
+if [ ! -e original/_FOSSIL_ ] ; then
+    mkdir -p original
+    cd original
+    fossil open ../original.fsl
+    cd ..
+fi
+
+if [ ! -e pulled.txt ] ; then
+    cd original
+    fossil pull | tee ../pulled.txt
+    cd ..
+fi
+
+cd original
+fossil update `tail -n1 ../previous.uuid`
+cd ..
+
+if true; then
+    for f in `cd original/src; ls *`; do
+	echo "========================================================"
+	echo $f
+	diff original/src/$f fossil/src/$f
+    done
+fi
+
