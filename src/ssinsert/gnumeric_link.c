04056fad479b679df0130dc8e04611660a472e3d
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2010-10-04 23:34:26 -0400

    fix up to work on modern gnumeric

--- src/ssinsert/gnumeric_link.c
+++ src/ssinsert/gnumeric_link.c
@@ -30,7 +30,9 @@
 #include <sys/resource.h>
 #endif
 
+#ifndef GNM_VERSION_FULL
 #define OLD_GNUMERIC
+#endif
 
 #ifdef OLD_GNUMERIC
 #define GOIOContext IOContext
@@ -39,6 +41,7 @@
 #define GNM_VERSION_FULL GNUMERIC_VERSION
 #define go_get_file_savers get_file_savers
 #define go_get_file_openers get_file_openers
+#define gnumeric_io_context_new go_io_context_new
 //#define go_ IOContext
 #endif
 
@@ -55,10 +58,11 @@ int gnumeric_init() {
 #ifdef OLD_GNUMERIC
 	gchar const **args = go_shell_argv_to_glib_encoding (argc, argv);
 	gnm_pre_parse_init (args[0]);
+	gnm_common_init (FALSE);
 #else
-	argv = gnm_pre_parse_init (argc, argv);
+	gnm_pre_parse_init (argc, argv);
+	gnm_init ();
 #endif
-  gnm_common_init (FALSE);
   
   cc = cmd_context_stderr_new ();
   gnm_plugins_init (GO_CMD_CONTEXT (cc));
@@ -72,14 +76,18 @@ static WorkbookView *wbv = NULL;
 
 int gnumeric_fini() {
   if (wbv!=NULL) {
+#ifdef OLD_GNUMERIC
     g_object_unref (wb_view_workbook (wbv));
+#else
+    g_object_unref (wb_view_get_workbook (wbv));
+#endif
   }
   g_object_unref (cc);
   gnm_shutdown ();
 }
 
 int gnumeric_load(const char *fname) {
-  IOContext *io_context = gnumeric_io_context_new (cc);
+  GOIOContext *io_context = go_io_context_new (cc);
   char *uri = go_filename_to_uri (fname);
   printf("Have uri %s\n", uri);
   wbv = wb_view_new_from_uri (uri, NULL,
@@ -104,7 +112,7 @@ int gnumeric_save(const char *fname) {
   g_print ("Using exporter %s\n",
 	   go_file_saver_get_id (fs));
   if (go_file_saver_get_save_scope (fs) !=
-      FILE_SAVE_WORKBOOK) {
+      GO_FILE_SAVE_WORKBOOK) {
     g_printerr (_("Selected exporter (%s) does not support saving multiple sheets in one file.\n"
 		  "Only the current sheet will be saved.\n"),
 		go_file_saver_get_id (fs));
@@ -130,69 +138,3 @@ int gnumeric_overlay_csv(const char *start, const char *stop) {
     return 0;
 }
 
-int gnumeric_convert (const char *in, const char *out) {
-  int res = 0;
-  GOFileSaver *fs = NULL;
-  GOFileOpener *fo = NULL;
-  char *infile = go_filename_to_uri (in);
-  char *outfile = go_filename_to_uri (out);
-
-  if (outfile != NULL) {
-    fs = go_file_saver_for_file_name (outfile);
-    if (fs == NULL) {
-      res = 2;
-      g_printerr (_("Unable to guess exporter to use for '%s'.\n"
-		    "Try --list-exporters to see a list of possibilities.\n"),
-		  outfile);
-      return 1;
-    }
-    g_print ("Using exporter %s\n",
-	     go_file_saver_get_id (fs));
-  }
-  fo = go_file_opener_for_id ("Gnumeric_stf:stf_csv");
-  if (fo) {
-    printf("Unknown opener\n");
-    return 1;
-  }
-  if (fs != NULL) {
-    printf("Start conversion...\n");
-    IOContext *io_context = gnumeric_io_context_new (cc);
-    char *uri = go_filename_to_uri (in);
-    printf("Have uri %s\n", uri);
-    WorkbookView *wbv = wb_view_new_from_uri (uri, fo,
-					      io_context, NULL);
-    g_free (uri);
-    printf("Have workbook view\n");
-
-    printf("For testing, try pasting some data into workbook\n");
-    //Workbook *wb = wb_view_get_workbook (wbv);
-    Sheet *sheet = wb_view_cur_sheet (wbv);
-    if (sheet==NULL) { printf("no sheet!\n"); return 1; }
-    char data[] = "42,fortytwo\n12,twelve\n";
-    char *start = &data[0];
-    char *stop = start+strlen(data);
-    StfParseOptions_t *options = stf_parse_options_guess(data);
-    stf_parse_sheet(options,start,stop,sheet,0,0);
-    stf_parse_options_free(options);
-    options = NULL;
-
-    if (go_file_saver_get_save_scope (fs) !=
-	FILE_SAVE_WORKBOOK) {
-      g_printerr (_("Selected exporter (%s) does not support saving multiple sheets in one file.\n"
-		    "Only the current sheet will be saved.\n"),
-		  go_file_saver_get_id (fs));
-    }
-    res = !wb_view_save_as (wbv, fs, outfile, cc);
-    g_object_unref (wb_view_workbook (wbv));
-    g_object_unref (io_context);
-  }
-  return res;
-}
-
-
-
-int hello() {
-  printf("Hello!\n");
-
-  return 0;
-}
