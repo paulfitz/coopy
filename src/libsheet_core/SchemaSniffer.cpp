18860217d17df7c4bb2c366e285d6b227d257b60
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2011-06-03 10:44:38 -0400

    add in primary key if needed

--- src/libsheet_core/SchemaSniffer.cpp
+++ src/libsheet_core/SchemaSniffer.cpp
@@ -2,10 +2,12 @@
 #include <coopy/NameSniffer.h>
 #include <coopy/IndexSniffer.h>
 #include <coopy/Dbg.h>
+#include <coopy/CompareFlags.h>
 
 #include <stdlib.h>
 
 using namespace coopy::store;
+using namespace coopy::cmp;
 
 using namespace std;
 
@@ -21,9 +23,34 @@ void SchemaSniffer::sniff(bool force) {
     }
   }
   NameSniffer nameSniffer(*sheet);
+  CompareFlags flags;
   vector<string> names = nameSniffer.suggestNames();
   vector<ColumnType> ct = nameSniffer.suggestTypes();
+  IndexSniffer indexSniffer(*sheet,flags,nameSniffer);
+  vector<int> indexes = indexSniffer.suggestIndexes();
+  bool haveKey = false;
+  bool shouldSetKey = true;
+  for (int i=0; i<(int)indexes.size(); i++) {
+    if (indexes[i]>0) {
+      haveKey = true;
+    }
+  }
+  for (int i=0; i<(int)ct.size(); i++) {
+    if (ct[i].primaryKeySet) {
+      shouldSetKey = false;
+      break;
+    }
+  }
+  if (haveKey&&shouldSetKey) {
+    for (int i=0; i<(int)indexes.size(); i++) {
+      ct[i].primaryKeySet = true;
+      ct[i].primaryKey = (indexes[i]>0);
+    }
+  }
   for (int i=0; i<(int)names.size(); i++) {
+    ColumnType base = ct[i];
+    if (haveKey) {
+    }
     fallback.addColumn(names[i].c_str(),ct[i]);
   }
   char col[3] = "A2";
