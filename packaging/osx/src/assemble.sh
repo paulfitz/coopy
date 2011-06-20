82afb2b1c564b6c47e5e91c9883c7f916e91b2a4
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2011-06-20 20:37:17 +0200

    working on 10.5

--- packaging/osx/src/assemble.sh
+++ packaging/osx/src/assemble.sh
@@ -34,11 +34,15 @@ mkdir -p Contents || exit 1
 cd Contents || exit 1
 mkdir -p MacOS || exit 1
 mkdir -p Resources || exit 1
+mkdir -p Frameworks || exit 1
 
 cp $SOURCE_DIR/conf/Info.plist $APP_DIR/Contents || exit 1
 cp $SOURCE_DIR/conf/coopy.icns $APP_DIR/Contents/Resources || exit 1
 
-for tool in coopy ssformat csvformat ssconvert; do
+coopy_bins=`cd $BUILD_DIR/homebrew/Cellar/coopy/*/bin/; ls`
+gnumeric_bins=`cd $BUILD_DIR/homebrew/Cellar/gnumeric/*/bin/; ls | grep -v 1`
+
+for tool in $coopy_bins $gnumeric_bins; do
 
   if [ ! -e "$BUNDLE_HOMEBREW/bin/$tool" ]; then
 	echo "Cannot find $BUNDLE_HOMEBREW/bin/$tool"
@@ -46,21 +50,20 @@ for tool in coopy ssformat csvformat ssconvert; do
   fi
 
   TARGET_DIR=$APP_DIR/Contents/MacOS
-  cp $BUNDLE_HOMEBREW/bin/$tool $TARGET_DIR || exit 1
+  cp -L $BUNDLE_HOMEBREW/bin/$tool $TARGET_DIR || exit 1
   cd $APP_DIR || exit 1
   chmod u+w $TARGET_DIR/$tool || exit 1
   for f in `cd $TARGET_DIR; otool -L $tool | egrep -v "[[:space:]]/usr/" | egrep -v "[[:space:]]/System" | sed "s/(.*//" | grep "/"`; do
 	echo "Processing [$f]"
         base=`basename $f`
-	f2=$TARGET_DIR/$base
+	f2=$TARGET_DIR/../Frameworks/$base
 	if [ ! -e $f2 ]; then
 	  cp $f $f2 || exit 1
 	fi
-        install_name_tool -change $f @executable_path/$base $TARGET_DIR/$tool || exit 1
+        install_name_tool -change $f @executable_path/../Frameworks/$base $TARGET_DIR/$tool || exit 1
 	chmod u+w $f2 || exit 1
-        echo install_name_tool -change $f @executable_path/$base $f2
-        install_name_tool -change $f @executable_path/$base $f2
-        install_name_tool -id @executable_path/$base $f2 || exit 1
+        install_name_tool -change $f @executable_path/../Frameworks/$base $f2
+        install_name_tool -id @executable_path/../Frameworks/$base $f2 || exit 1
 	otool -L $f2 | grep $f && exit 1
 	chmod u-w $f2 || exit 1
   done
@@ -69,40 +72,43 @@ for tool in coopy ssformat csvformat ssconvert; do
 done
 
 for rep in 1 2 3 4 5; do
-for f0 in `cd $TARGET_DIR; ls *.dylib`; do
-  for f in `cd $TARGET_DIR; otool -L $f0 | egrep -v "[[:space:]]/usr/" | egrep -v "[[:space:]]/System" | sed "s/(.*//" | grep "/"`; do
+for f0 in `cd $TARGET_DIR/../Frameworks; ls *.dylib`; do
+  for f in `cd $TARGET_DIR/../Frameworks; otool -L $f0 | egrep -v "[[:space:]]/usr/" | egrep -v "[[:space:]]/System" | sed "s/(.*//" | grep "/"`; do
 	echo "Processing [$f]"
         base=`basename $f`
-	f2=$TARGET_DIR/$base
+	f2=$TARGET_DIR/../Frameworks/$base
 	if [ ! -e $f2 ]; then
 	  cp $f $f2 || exit 1
 	fi
-	chmod u+w $TARGET_DIR/$f0 || exit 1
-        install_name_tool -change $f @executable_path/$base $TARGET_DIR/$f0 || exit 1
-	chmod u-w $TARGET_DIR/$f0 || exit 1
+	chmod u+w $TARGET_DIR/../Frameworks/$f0 || exit 1
+        install_name_tool -change $f @executable_path/../Frameworks/$base $TARGET_DIR/../Frameworks/$f0 || exit 1
+	chmod u-w $TARGET_DIR/../Frameworks/$f0 || exit 1
   done
 done
 done
 
-# echo "Copying libxml2.2.dylib"
-# cp $BUILD_DIR/homebrew/Cellar/libxml2/2.*/lib/libxml2.2.dylib $TARGET_DIR/libxml2.2.dylib || exit 1
-# echo "Copied libxml2.2.dylib"
-#for f0 in `cd $TARGET_DIR; ls`; do
-#        echo "Fixing [$f0]"
-#	chmod u+w $TARGET_DIR/$f0 || exit 1
-#       install_name_tool -change /usr/lib/libxml2.2.dylib @executable_path/libxmllocal2.2.dylib $TARGET_DIR/$f0 || exit 1
-#	chmod u-w $TARGET_DIR/$f0 || exit 1
-#done
-#mv $TARGET_DIR/libxml2.2.dylib $TARGET_DIR/libxmllocal2.2.dylib
-#chmod u+w $TARGET_DIR/libxmllocal2.2.dylib
-#install_name_tool -id @executable_path/libxmllocal2.2.dylib $TARGET_DIR/libxmllocal2.2.dylib || exit 1
-#chmod u-w $TARGET_DIR/libxmllocal2.2.dylib
+cp -R -L $BUNDLE_HOMEBREW/etc $TARGET_DIR/../Frameworks/etc || exit 1
+mkdir -p $TARGET_DIR/../Frameworks/lib || exit 1
+cp -R -L $BUNDLE_HOMEBREW/lib/gdk-pixbuf-2.0 $TARGET_DIR/../Frameworks/lib || exit 1
+cp -R -L $BUNDLE_HOMEBREW/lib/gnumeric $TARGET_DIR/../Frameworks/lib || exit 1
+cp -R -L $BUNDLE_HOMEBREW/lib/goffice $TARGET_DIR/../Frameworks/lib || exit 1
+cp -R -L $BUNDLE_HOMEBREW/lib/pango $TARGET_DIR/../Frameworks/lib || exit 1
 
-cp -R -L $BUNDLE_HOMEBREW/etc $TARGET_DIR/etc || exit 1
-mkdir -p $TARGET_DIR/lib || exit 1
-cp -R -L $BUNDLE_HOMEBREW/lib/gdk-pixbuf-2.0 $TARGET_DIR/lib || exit 1
-cp -R -L $BUNDLE_HOMEBREW/lib/gnumeric $TARGET_DIR/lib || exit 1
-cp -R -L $BUNDLE_HOMEBREW/lib/goffice $TARGET_DIR/lib || exit 1
-cp -R -L $BUNDLE_HOMEBREW/lib/pango $TARGET_DIR/lib || exit 1
+for f0 in `cd $TARGET_DIR/../Frameworks/lib; find . -iname "*.so"`; do
+  for f in `cd $TARGET_DIR/../Frameworks/lib; otool -L $f0 | egrep -v "[[:space:]]/usr/" | egrep -v "[[:space:]]/System" | sed "s/(.*//" | grep "/"`; do
+	echo "Processing [$f]"
+        base=`basename $f`
+	chmod u+w $TARGET_DIR/../Frameworks/lib/$f0 || exit 1
+        install_name_tool -change $f @executable_path/../Frameworks/$base $TARGET_DIR/../Frameworks/lib/$f0 || exit 1
+	chmod u-w $TARGET_DIR/../Frameworks/lib/$f0 || exit 1
+  done
+done
+
+sed -i "" "s|/.*lib/|@executable_path/../Frameworks/lib/|" $TARGET_DIR/../Frameworks/etc/pango/pango.modules || exit 1
+(
+echo "[Pango]"
+echo "ModuleFiles = @executable_path/../Frameworks/etc/pango/pango.modules"
+) > $TARGET_DIR/../Frameworks/etc/pango/pango.rc
+
 cd $BUILD_DIR || exit 1
 zip -r coopy.app.zip coopy.app || exit 1
