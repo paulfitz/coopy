adc040800a19499864f0ce02642d26f40946fe82
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2011-08-16 12:09:58 -0400

    document main namespaces

---
+++ src/libsheet_core/include/coopy/Doc.h
@@ -0,0 +1,37 @@
+#ifndef COOPY_DOC_INC
+#define COOPY_DOC_INC
+
+/**
+ *
+ * All COOPY toolbox classes.
+ *
+ */
+namespace coopy {
+
+  /**
+   *
+   * Comparison between tables.
+   *
+   */
+  namespace cmp {
+  }
+
+  /**
+   *
+   * Format descriptions of tables.
+   *
+   */
+  namespace format {
+  }
+
+
+  /**
+   *
+   * Storage for tables.
+   *
+   */
+  namespace store {
+  }
+}
+
+#endif
