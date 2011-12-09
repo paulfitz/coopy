8cc58cef65521d32bc877d134676ee2f602150a7
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2011-12-09 17:17:42 +0100

    fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+if [ ! -e CMakeCache.txt ]; then
+    echo "Call from build directory"
+    exit 1
+fi
+
+BASE="$PWD"
+SRC=`cat coopy_source.txt`
+
 echo "I keep forgetting how to release"
-echo "  ./scripts/release.sh package"
-echo "  ./scripts/release.sh tag"
+echo "  ../scripts/release.sh package"
+echo "  ../scripts/release.sh tag"
 
 if [ "k$1" = "kpackage" ]; then
-    ./scripts/make_packages.sh linux || exit 1
-    ./scripts/make_packages.sh mingw || exit 1
-    ./scripts/make_packages.sh osx || exit 1
-    ./scripts/make_packages.sh doc || exit 1
+    $SRC/scripts/make_packages.sh linux || exit 1
+    $SRC/scripts/make_packages.sh mingw || exit 1
+    $SRC/scripts/make_packages.sh osx || exit 1
+    $SRC/scripts/make_packages.sh doc || exit 1
 fi
 
 if [ "k$1" = "ktag" ]; then
-  cd build && cmake . || exit 1
+  cmake . || exit 1
   tag=`cat coopy_version.txt`
   echo "tag is $tag, ok?"
   read x
